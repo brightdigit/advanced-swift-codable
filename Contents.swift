180c3741d867231c9bfa6c45ae4e8b8c038989da
Leo Dion <leogdion@brightdigit.com>
2019-01-30 20:36:55 -0500

    twitter deserialization

--- Contents.swift
+++ Contents.swift
@@ -74,11 +74,65 @@ public struct Tweet : Codable {
   public let in_reply_to_user_id : Int?
   public let in_reply_to_screen_name : String
   public let user : TweetUser
+  public let quoted_status : QuotedTweet
+  public let is_quote_status : Bool
+  public let retweet_count : Int
+  public let favorite_count : Int
+  public let favorited : Bool
+  public let retweeted : Bool
+  public let possibly_sensitive : Bool
+  public let possibly_sensitive_appealable : Bool
+  public let lang : String
 }
+
+public struct QuotedTweet : Codable {
+  public let created_at : Date
+  public let id : Int
+  public let full_text : String
+  public let display_text_range : [Int]
+  public let entities : TweetEntities
+  public let user : TweetUser
+  public let source : String
+  public let extended_entities : TweetEntities
+  public let is_quote_status : Bool
+  public let retweet_count : Int
+  public let favorite_count : Int
+  public let favorited : Bool
+  public let retweeted : Bool
+  public let possibly_sensitive : Bool
+  public let possibly_sensitive_appealable : Bool
+  public let lang : String
+}
+
+public struct TweetMediaEntitySizeSet : Codable {
+  public let medium : TweetMediaEntitySize
+  public let thumb : TweetMediaEntitySize
+  public let large : TweetMediaEntitySize
+  public let small : TweetMediaEntitySize
+}
+
+public struct TweetMediaEntitySize : Codable {
+  public let w : Int
+  public let h : Int
+  public let resize : String
+}
+public struct TweetMediaEntity : Codable {
+  public let id : Int
+  public let indices : [Int]
+  public let media_url : URL
+  public let media_url_https : URL
+  public let url : URL
+  public let display_url : String
+  public let expanded_url : URL
+  public let type : String
+  public let sizes : TweetMediaEntitySizeSet
+}
+
 
 public struct TweetEntities : Codable {
   public let user_mentions : [TweetUserMentionsEntity]?
   public let urls : [TweetURL]?
+  public let media : [TweetMediaEntity]?
 }
 
 public struct TweetUserMentionsEntity : Codable {
@@ -99,169 +153,3 @@ let decoder = JSONDecoder()
 decoder.dateDecodingStrategy = .formatted(twitterDateFormatter)
 let tweet = try decoder.decode(Tweet.self, from: twitterData)
 
-
-//  "geo": null,
-//  "coordinates": null,
-//  "place": null,
-//  "contributors": null,
-//  "quoted_status_id": 704059336788606976,
-//  "quoted_status_id_str": "704059336788606976",
-//  "quoted_status": {
-//    "created_at": "Sun Feb 28 21:43:21 +0000 2016",
-//    "id": 704059336788606976,
-//    "id_str": "704059336788606976",
-//    "full_text": "My favorite photographic subject, up closer than ever before. https://t.co/K958bKh9Sd",
-//    "display_text_range": [
-//    0,
-//    85
-//    ],
-//    "entities": {
-//      "hashtags": [],
-//      "symbols": [],
-//      "user_mentions": [],
-//      "urls": [],
-//      "media": [
-//      {
-//      "id": 704059330149031936,
-//      "id_str": "704059330149031936",
-//      "indices": [
-//      62,
-//      85
-//      ],
-//      "media_url": "http://pbs.twimg.com/media/CcVSOwJVIAAKwE6.jpg",
-//      "media_url_https": "https://pbs.twimg.com/media/CcVSOwJVIAAKwE6.jpg",
-//      "url": "https://t.co/K958bKh9Sd",
-//      "display_url": "pic.twitter.com/K958bKh9Sd",
-//      "expanded_url": "http://twitter.com/jeremycloud/status/704059336788606976/photo/1",
-//      "type": "photo",
-//      "sizes": {
-//      "medium": {
-//      "w": 600,
-//      "h": 600,
-//      "resize": "fit"
-//      },
-//      "thumb": {
-//      "w": 150,
-//      "h": 150,
-//      "resize": "crop"
-//      },
-//      "large": {
-//      "w": 871,
-//      "h": 871,
-//      "resize": "fit"
-//      },
-//      "small": {
-//      "w": 340,
-//      "h": 340,
-//      "resize": "fit"
-//      }
-//      }
-//      }
-//      ]
-//    },
-//    "extended_entities": {
-//      "media": [
-//      {
-//      "id": 704059330149031936,
-//      "id_str": "704059330149031936",
-//      "indices": [
-//      62,
-//      85
-//      ],
-//      "media_url": "http://pbs.twimg.com/media/CcVSOwJVIAAKwE6.jpg",
-//      "media_url_https": "https://pbs.twimg.com/media/CcVSOwJVIAAKwE6.jpg",
-//      "url": "https://t.co/K958bKh9Sd",
-//      "display_url": "pic.twitter.com/K958bKh9Sd",
-//      "expanded_url": "http://twitter.com/jeremycloud/status/704059336788606976/photo/1",
-//      "type": "photo",
-//      "sizes": {
-//      "medium": {
-//      "w": 600,
-//      "h": 600,
-//      "resize": "fit"
-//      },
-//      "thumb": {
-//      "w": 150,
-//      "h": 150,
-//      "resize": "crop"
-//      },
-//      "large": {
-//      "w": 871,
-//      "h": 871,
-//      "resize": "fit"
-//      },
-//      "small": {
-//      "w": 340,
-//      "h": 340,
-//      "resize": "fit"
-//      }
-//      }
-//      }
-//      ]
-//    },
-//    "truncated": false,
-//    "source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
-//    "in_reply_to_status_id": null,
-//    "in_reply_to_status_id_str": null,
-//    "in_reply_to_user_id": null,
-//    "in_reply_to_user_id_str": null,
-//    "in_reply_to_screen_name": null,
-
-//    "user": {
-
-//      "protected": false,
-//      "followers_count": 4324,
-//      "friends_count": 410,
-//      "listed_count": 103,
-//      "created_at": "Mon Jun 09 17:00:58 +0000 2008",
-//      "favourites_count": 815,
-//      "utc_offset": -18000,
-//      "time_zone": "Central Time (US & Canada)",
-//      "geo_enabled": true,
-//      "verified": false,
-//      "statuses_count": 2218,
-//      "lang": "en",
-//      "contributors_enabled": false,
-//      "is_translator": false,
-//      "is_translation_enabled": false,
-//      "profile_background_color": "000000",
-//      "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-//      "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-//      "profile_background_tile": false,
-//      "profile_image_url": "http://pbs.twimg.com/profile_images/436903139183054849/i_MbCcoW_normal.jpeg",
-//      "profile_image_url_https": "https://pbs.twimg.com/profile_images/436903139183054849/i_MbCcoW_normal.jpeg",
-//      "profile_banner_url": "https://pbs.twimg.com/profile_banners/15062340/1447451621",
-//      "profile_link_color": "4A913C",
-//      "profile_sidebar_border_color": "000000",
-//      "profile_sidebar_fill_color": "000000",
-//      "profile_text_color": "000000",
-//      "profile_use_background_image": false,
-//      "has_extended_profile": true,
-//      "default_profile": false,
-//      "default_profile_image": false,
-//      "following": true,
-//      "follow_request_sent": false,
-//      "notifications": false
-//    },
-//    "geo": null,
-//    "coordinates": null,
-//    "place": null,
-//    "contributors": null,
-//    "is_quote_status": false,
-//    "retweet_count": 0,
-//    "favorite_count": 11,
-//    "favorited": false,
-//    "retweeted": false,
-//    "possibly_sensitive": false,
-//    "possibly_sensitive_appealable": false,
-//    "lang": "en"
-//  },
-//  "is_quote_status": true,
-//  "retweet_count": 0,
-//  "favorite_count": 0,
-//  "favorited": false,
-//  "retweeted": false,
-//  "possibly_sensitive": false,
-//  "possibly_sensitive_appealable": false,
-//  "lang": "en"
-//}
